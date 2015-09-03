a2553e7d94571a214fe79ccc0bd65e62d6400645
John Hardy <jh@lagado.com>
2015-09-03 21:44:49 +1000

    ajax

--- xdloader.js
+++ xdloader.js
@@ -21,7 +21,7 @@
     lib.create = create;
     
     var remotes = {};
-    var deferreds = {};
+    var pending = {};
     
     var body = document.getElementsByTagName('body')[0];
     
@@ -29,37 +29,27 @@
         var remote = remotes[e.origin];
         if (!remote)
             return;
-        var msg = JSON.parse(e.data);
-        if (msg.name === 'remoteready') {
+        var response = JSON.parse(e.data);
+        if (response.name === 'remoteready') {
             if (remote.deferred) {
                 remote.source = e.source;
                 remote.deferred.resolve(remote);
                 remote.deferred = null;
             }
         } 
-        else if (msg.name == 'load.response') {
-            var def = deferreds[msg.path];
-            deferreds[msg.path] = null;
-            if (def) {
-                if (msg.result == 'ok')
-                    def.resolve(msg.data);
-                else
-                    def.reject(msg.result);
-            }
-        }
-        else if (msg.name == 'ajax.response') {
-            var def = deferreds[msg.id];
-            deferreds[msg.id] = null;
-            if (def) {
-                var result = {
-                    data: msg.data,
-                    statuscode: msg.statuscode,
-                }
-                if (msg.statuscode == 200)
-                    def.resolve(result);
+        else if (response.name == 'ajax.response') {
+            var req = pending[response.id];
+            pending[response.id] = null;
+            if (req) {
+                if (response.status == 200) {
+                    
+                    if (req.props.json)
+                        response.data = JSON.parse(response.responseText);
+                    
+                    req.def.resolve(response);
+                } 
                 else {
-                    result.error = msg.error;
-                    def.reject(result);
+                    req.def.reject(response);
                 }
             }
         }
@@ -70,8 +60,9 @@
         parser.href = loaderurl;
         var origin = parser.protocol + '//' + parser.hostname;
         var path = parser.pathname;
-        if (path[0] != '/') path = '/' + path;
-
+        if (path[0] != '/')
+            path = '/' + path;
+        
         var remote = remotes[origin];
         if (remote)
             return remote;
@@ -82,10 +73,11 @@
     
     function Remote(origin, path, timeout) {
         
-        if (!timeout) timeout = 10000;
-
+        if (!timeout)
+            timeout = 10000;
+        
         this.deferred = $.Deferred();
-
+        
         var element = document.createElement('iframe');
         element.src = origin + path;
         element.width = '1';
@@ -95,50 +87,40 @@
         element.scrolling = 'no';
         
         body.appendChild(element);
-
+        
         var remote = this;
-        setTimeout(function(){
+        setTimeout(function() {
             if (remote.deferred) {
                 remote.deferred.reject('Could not create remote');
                 remote.deferred = null;
             }
         }, timeout);
         
-        this.load = function(path) {
+        this.ajax = function(method, path, data, json) {
             if (!this.source)
                 return;
-            var def = deferreds[path];
-            if (!def) {
-                def = $.Deferred();
-                deferreds[path] = def;
-                var msg = JSON.stringify({
-                    command: 'load',
-                    path: path,
-                });
-                this.source.postMessage(msg, origin);
-            }
-            return def.promise();
-        }
-        
-        this.ajax = function(method, path, data) {
-            if (!this.source)
-                return;
-
-            var id = String(Date.now());    
+            
+            var id = String(Math.random());
+            if (pending[id]) return;
+            
             var def = $.Deferred();
-            deferreds[id] = def;
-            var msg = {
+            var props = {
                 command: 'ajax',
                 id: id,
                 method: method,
                 path: path,
                 data: data,
+                json: json !== false,
             }
-
-            this.source.postMessage(JSON.stringify(msg), origin);
+            pending[id] = {
+                def: def,
+                props: props,
+            };
+            
+            this.source.postMessage(JSON.stringify(props), origin);
             return def.promise();
         }
-
+        
         this.destroy = function() {
             body.removeElement(remote.element);
             remotes[origin] = null;
